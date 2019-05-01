d16a02220b53fde276f194fff44b40b2da4d7143
Davide Serafini <davide@davideserafini.com>
2019-05-01 18:55:55 +0200

    Added json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormFieldConfig } from './models/form-field-config';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   formValues: string;
+
+  formConfigurationJson: FormFieldConfig[] = [{
+    label: 'Name',
+    type: 'input',
+    name: 'name',
+    value: '',
+    inputType: 'text',
+    placeholder: 'FVG Dev',
+    validations: [{
+      name: 'required',
+      message: 'Name is required'
+    }, {
+      name: 'minlength',
+      message: 'Name is too short',
+      value: 5
+    }],
+  }, {
+    label: 'Email',
+    type: 'input',
+    name: 'email',
+    value: '',
+    inputType: 'email',
+    placeholder: 'community@fvg.dev',
+    validations: [{
+      name: 'required',
+      message: 'Email is required'
+    }, {
+      name: 'email',
+      message: 'Email format is not correct'
+    }],
+  }, {
+    label: 'Role',
+    type: 'select',
+    name: 'role',
+    value: 'user',
+    options: [{
+      value: 'user',
+      text: 'User'
+    }, {
+      value: 'admin',
+      text: 'Admin'
+    }]
+  }, {
+    label: '',
+    type: 'input',
+    inputType: 'radio',
+    name: 'gender',
+    value: '',
+    options: [{
+      value: 'm',
+      text: 'Male'
+    }, {
+      value: 'f',
+      text: 'Female'
+    }]
+  }, {
+    label: 'Submit',
+    type: 'button',
+    name: 'submit',
+    value: ''
+  }];
 
   displayValues(values) {
     this.formValues = values;
