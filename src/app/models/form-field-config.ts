f0620c36fba5e156c8d7ecbb480226ec1e672b22
Davide Serafini <davide@davideserafini.com>
2019-05-01 18:55:38 +0200

    Added models

---
+++ src/app/models/form-field-config.ts
@@ -0,0 +1,18 @@
+import { Option } from './option';
+import { Validation } from './validation';
+
+export interface FormFieldConfig {
+  // Fields for any form field
+  label: string;
+  type: string;
+  name: string;
+  value: string;
+  placeholder?: string;
+  validations?: Validation[];
+
+  // Fields for inputs
+  inputType?: string;
+
+  // Fields for selects, input radio
+  options?: Option[];
+}
